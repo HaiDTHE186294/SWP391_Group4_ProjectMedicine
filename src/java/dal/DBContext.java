2396357e1365134cf3f2d95436ad5499cfec88f2
anhttn-he186474 <anhttnhe186474@fpt.edu.vn>
2024-10-15 12:40:12 +0700

    add code

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -18,10 +18,8 @@ public class DBContext {
     public DBContext() {
         try {
             String user = "sa";
-
             String pass = "123456";
             String url = "jdbc:sqlserver://localhost\\MSSQLSERVER:1433;databaseName=SWP";
-
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
