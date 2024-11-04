12c3887b55b5881da240e90d9684da08a14501de
anhttn-he186474 <anhttnhe186474@fpt.edu.vn>
2024-11-04 12:11:44 +0700

    Apply gitignore and DBContext

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -20,9 +20,9 @@ public class DBContext {
             String user = "sa";
 
 
-            String pass = "123";
+            String pass = "123456";
 
-            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=SWP1";
+            String url = "jdbc:sqlserver://localhost\\MSSQLSERVER:1433;databaseName=SWP1";
 
 
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
